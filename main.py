13c89cfa8996c7b9f4a495a4c05e6d401cf0066e
sergVe <testfrontol@gmail.com>
2022-02-09 00:52:24 +0300

    publish text to Telegram channel

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ from dotenv import load_dotenv
 from telegram.ext import Updater, ExtBot
 
 
+def add_0(item: str):
+    return item if len(item) == 2 else f'0{item}'
+
+
 def get_response(url, options=None):
     headers = {
         'User-Agent': 'my-first-bot / 1.0'
     }
-
     response = requests.get(url, headers=headers, params=options)
     response.raise_for_status()
     return response
@@ -45,10 +48,8 @@ def fetch_spacex_last_launch(directory, url):
                 break
 
     for i_link in images_links:
-        print(i_link)
         parsed_link = urlparse(i_link)
-        test_path = parsed_link.path
-        start = test_path.rfind('/') + 1
+        start = parsed_link.path.rfind('/') + 1
         filename = parsed_link.path[start:]
         path = os.path.abspath(os.path.join(directory, filename))
         download_image(path, i_link)
@@ -65,7 +66,7 @@ def fetch_nasa_images(directory, url):
     for i, i_link in enumerate(images_links):
         filename = f'nasa_{i + 1}{get_file_extension(i_link)}'
         path = os.path.abspath(os.path.join(directory, filename))
-        download_image(path, i_link)
+        download_image(path, i_link, query_params)
 
 
 def fetch_nasa_epic_images(directory, url):
@@ -75,15 +76,15 @@ def fetch_nasa_epic_images(directory, url):
     }
     response = get_response(url, query_params)
     limit_response = response.json()[:10]
+
     for i, response_unit in enumerate(limit_response):
         for k, v in response_unit.items():
             if k == 'date':
                 image_date = datetime.datetime.fromisoformat(v)
             if k == 'image':
                 image_name = v
-        image_url = f'https://api.nasa.gov/EPIC/archive/natural/' \
-                    f'{image_date.year}/{image_date.month}/{image_date.day}' \
-                    f'/png/{image_name}.png'
+        image_url = f'https://api.nasa.gov/EPIC/archive/natural/{image_date.year}/{add_0(str(image_date.month))}' \
+                    f'/{add_0(str(image_date.day))}/png/{image_name}.png'
 
         filename = f'nasa_epic_{i + 1}.png'
         path = os.path.abspath(os.path.join(directory, filename))
@@ -93,8 +94,8 @@ def fetch_nasa_epic_images(directory, url):
 def main():
     url_spacex = 'https://api.spacexdata.com/v4/launches/'
     directory = 'images'
-    nasa_directory = 'nasa_directory'
-    nasa_epic_directory = 'nasa_epic_directory'
+    # nasa_directory = 'nasa_directory'
+    # nasa_epic_directory = 'nasa_epic_directory'
     nasa_url = 'https://api.nasa.gov/planetary/apod'
     nasa_epic_url = 'https://api.nasa.gov/EPIC/api/natural'
 
@@ -102,20 +103,21 @@ def main():
         load_dotenv()
         if not os.path.exists(directory):
             os.makedirs(directory)
-        if not os.path.exists(nasa_directory):
-            os.makedirs(nasa_directory)
-        if not os.path.exists(nasa_epic_directory):
-            os.makedirs(nasa_epic_directory)
 
         # fetch_spacex_last_launch(directory, url_spacex)
-        # fetch_nasa_images(nasa_directory, nasa_url)
-        # fetch_nasa_epic_images(nasa_epic_directory, nasa_epic_url)
-        tkn = os.getenv('TELEGRAM_KEY')
-        bot = ExtBot(token=os.getenv('TELEGRAM_KEY'))
-        # updates = bot.getUpdates()
-        updates = bot.get_me()
-        print(updates)
-        bot.send_message(chat_id=os.getenv('CHAT_ID'), text="I'm sorry Dave I'm afraid I can't do that.")
+        # fetch_nasa_images(directory, nasa_url)
+        # fetch_nasa_epic_images(directory, nasa_epic_url)
+        telegram_token = os.getenv('TELEGRAM_KEY')
+        chat_id = os.getenv('CHAT_ID')
+        bot = ExtBot(token=telegram_token)
+        bot.send_message(chat_id, 'Hi!')
+        with open('/home/serge/PycharmProjects/training/devman/dvnm-api-4/images/nasa_epic_9.png', mode='rb') as photo:
+            bot.send_photo(chat_id, photo)
+        # updater = Updater(telegram_token, use_context=True)
+        # dispatcher = updater.dispatcher
+        # updater.start_polling()
+        # updater.idle()
+
 
     except (requests.exceptions.HTTPError,
             requests.exceptions.InvalidSchema,
